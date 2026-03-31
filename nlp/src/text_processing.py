61889b7650f8c886055ece61b20056f1ee113696
JonnerPaz <jonnerpazp@gmail.com>
2026-03-31 12:08:25 -0400

    learn about text_processing

--- nlp/src/text_processing.py
+++ nlp/src/text_processing.py
@@ -6,15 +6,35 @@ import re
 import pandas as pd
 from pathlib import Path
 
+nltk.download("stopwords")
+
 csv = Path("../txt_files/tripadvisor_hotel_reviews.csv")
+print("my path:", csv)
 
 data = pd.read_csv(csv)
-# data.info()
 
-df = data.head()
-df["review_lowercase"] = df["Review"].str.lower()
+# df = data.head()
+# print(df["Review"][0])
+
+# when creating new rows, use the data object, not the dataframe (df)
+data["review_lowercase"] = data["Review"].str.lower()
+
+# add new column with stopwords removed
 en_stopwords = stopwords.words("english")
 en_stopwords.remove("not")
-df["review_no_stopwords"] = df["review_lowercase"].apply(
+data["review_no_stopwords"] = data["review_lowercase"].apply(
     lambda x: " ".join([word for word in x.split() if word not in en_stopwords])
 )
+
+# using regex to cleanup
+# axis: tells go to the data row by row
+data["review_no_stopwords_no_punct"] = data.apply(
+    lambda x: re.sub(r"[*]", "star", x["review_no_stopwords"]), axis=1
+)
+
+# remove punctuation
+data["review_no_stopwords_no_punct"] = data.apply(
+    lambda x: re.sub(r"[^\w\s]", "", x["review_no_stopwords_no_punct"]), axis=1
+)
+
+print(data["review_no_stopwords_no_punct"])
