92b6f0c35c72695a30655ffe5fe669e59d5b9a7e
JonnerPaz <jonnerpazp@gmail.com>
2026-04-21 22:08:14 -0400

    feat: Add statistics module

--- aifromscratchbook/statistics/describe.py
+++ aifromscratchbook/statistics/describe.py
@@ -1,4 +1,7 @@
 from collections import Counter
+import math
+from linear_algebra.vectors import sum_of_squares
+from linear_algebra.vectors import dot
 import matplotlib.pyplot as plt
 
 
@@ -45,3 +48,44 @@ def mode(v: list[float]) -> list[float]:
     counts = Counter(v)
     max_count = max(counts.values())
     return [x for x, count in counts.items() if count == max_count]
+
+
+def data_range(v: list[float]) -> float:
+    return max(v) - min(v)
+
+
+def de_average(xs: list[float]) -> list[float]:
+    """Translate xs by subtracting its mean (so the result has mean 0)"""
+    x_bar = average(xs)
+    return [x - x_bar for x in xs]
+
+
+def variance(xs: list[float]) -> float:
+    """Almost the average squared distance from the mean"""
+    assert len(xs) >= 2, "variance requires at least two elements"
+
+    n = len(xs)
+    deviations = de_average(xs)
+    return sum_of_squares(deviations) / (n - 1)
+
+
+def standard_deviation(xs: list[float]) -> float:
+    return math.sqrt(variance(xs))
+
+
+def interquartile_range(v: list[float]) -> float:
+    return quantile(v, 0.75) - quantile(v, 0.25)
+
+
+def covariance(xs: list[float], ys: list[float]) -> float:
+    assert len(xs) == len(ys), "xs and ys must be the same length"
+    return dot(de_average(xs), de_average(ys)) / (len(xs) - 1)
+
+
+def correlation(xs: list[float], ys: list[float]) -> float:
+    stdev_x = standard_deviation(xs)
+    stdev_y = standard_deviation(ys)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(xs, ys) / stdev_x / stdev_y
+    else:
+        return 0
